b35ba13f77f804ce0b5180bc709fe576b2552ce2
HAOYUatHZ <haoyu@protonmail.com>
2024-02-23 15:38:35 +0800

    draft

--- main.go
+++ main.go
@@ -11,8 +11,18 @@ var l2GethEndpoint = "http://localhost:8545"
 func main() {
 	ctx := context.Background()
 
-	_, err := ethclient.DialContext(ctx, l2GethEndpoint)
+	l2GethClient, err := ethclient.DialContext(ctx, l2GethEndpoint)
 	if err != nil {
 		panic(err)
 	}
+	// Use gzip compression.
+	l2GethClient.SetHeader("Accept-Encoding", "gzip")
+
+	// GetNumSkippedTransactions
+
+	// GetSkippedTransactionHashes
+
+	// GetSkippedTransaction
+
+	// GetTxBlockTraceOnTopOfBlock
 }
